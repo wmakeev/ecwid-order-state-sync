1db4c50a614b62e3488c144f729dd76ecdac460a
Vitaliy V. Makeev <w.makeev@gmail.com>
2021-11-23 16:56:10 +0500

    env: remove test error

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 export function some() {
   const a = 2 + 3
 
-  console.log(process.env.NODE_OPTIONS)
-
-  throw new Error(`My error, ${a}`)
-}
-
-try {
-  some()
-} catch (err) {
-  if (err instanceof Error) {
-    console.log(err.stack)
-  }
+  console.log(a)
 }
